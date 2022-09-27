6181c1ff9d3a625cb33a4382adcdbd1bd17d3855
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-09-27 19:57:19 +0200

    Cleanup

--- app/src/main/java/com/matis/movieapp/ui/viewmodels/SearchViewModel.kt
+++ app/src/main/java/com/matis/movieapp/ui/viewmodels/SearchViewModel.kt
@@ -1,15 +1,12 @@
 package com.matis.movieapp.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.matis.movieapp.data.models.discoverMovies.Result
 import com.matis.movieapp.data.sources.SearchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
