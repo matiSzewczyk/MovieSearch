dacbdf467c911d5d327cd5b5d48560a8e8b22734
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-10-01 15:56:16 +0200

    Cleanup

--- app/src/main/java/com/matis/movieapp/data/sources/SearchApi.kt
+++ app/src/main/java/com/matis/movieapp/data/sources/SearchApi.kt
@@ -4,13 +4,10 @@ import com.matis.movieapp.BuildConfig
 import com.matis.movieapp.data.models.discoverMovies.DiscoverMovies
 import retrofit2.Response
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.Headers
 
 interface SearchApi {
 
-    // This endpoint requires the app to be verified by Allegro.
-    // Maybe take another look at this later down the road.
     @Headers("Authorization: Bearer ${BuildConfig.API_KEY}")
     @GET(
         "discover/movie?language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate" +
