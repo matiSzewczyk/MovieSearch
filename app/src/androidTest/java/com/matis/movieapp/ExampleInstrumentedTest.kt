b0c13e70dfbde64c405c00afb755f5b7a3564955
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-10-01 15:53:45 +0200

    Fix project naming

--- app/src/androidTest/java/com/matis/movieapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/matis/movieapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.matis.allegroapi", appContext.packageName)
+        assertEquals("com.matis.movieapp", appContext.packageName)
     }
 }
