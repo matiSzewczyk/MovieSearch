679944520b18fd62cfb76c3bac059c87a69cd6a0
matiSzewczyk <szewczyk.mateusz16@gmail.com>
2022-10-08 16:38:38 +0200

    Rename fragment to DetailsFragment

--- app/src/main/java/com/matis/movieapp/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/matis/movieapp/ui/fragments/DetailsFragment.kt
@@ -5,12 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.bumptech.glide.Glide
-import com.matis.movieapp.databinding.FragmentMovieBinding
+import com.matis.movieapp.databinding.FragmentDetailsBinding
 
-class MovieFragment : Fragment() {
+class DetailsFragment : Fragment() {
 
-    private var _binding: FragmentMovieBinding? = null
+    private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -18,7 +17,7 @@ class MovieFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentMovieBinding.inflate(inflater, container, false)
+        _binding = FragmentDetailsBinding.inflate(inflater, container, false)
         return binding.root
     }
 
